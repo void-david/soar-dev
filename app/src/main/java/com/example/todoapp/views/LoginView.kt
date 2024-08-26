6e90c98295ec288c2d7259490cf9e6bba42ae04b
David A. Lozano <void9david@gmail.com>
2024-08-26 15:48:10 -0600

    added image to login

--- app/src/main/java/com/example/todoapp/views/LoginView.kt
+++ app/src/main/java/com/example/todoapp/views/LoginView.kt
@@ -1,12 +1,15 @@
 package com.example.todoapp.views
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -18,35 +21,44 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.todoapp.R
 
 @Composable
 fun LoginView(navController: NavHostController){
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var loginError by remember { mutableStateOf(false) }
+    var imageLegal = painterResource(R.drawable.user_icon_on_transparent_background_free_png)
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(top = 100.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Top
     ) {
+        Image(painter = imageLegal, contentDescription = "Legal Hoy Logo",
+            modifier = Modifier.width(300.dp)
+                                .height(300.dp)
+                                .padding(bottom = 25.dp)
+        )
         TextField(
             value = username,
             onValueChange = { username = it },
             label = { Text("Username") },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.width(380.dp)
         )
         Spacer(modifier = Modifier.height(8.dp))
         TextField(
             value = password,
             onValueChange = { password = it },
             label = { Text("Password") },
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.width(380.dp),
             visualTransformation = PasswordVisualTransformation()
         )
         Spacer(modifier = Modifier.height(16.dp))
@@ -56,7 +68,13 @@ fun LoginView(navController: NavHostController){
             Spacer(modifier = Modifier.height(8.dp))
         }
 
-        Button(onClick = {
+        Button(modifier = Modifier
+            .width(300.dp)
+            .height(60.dp) ,
+
+            //border radius
+
+            onClick = {
             navController.navigate("dashboard")
         }) {
             Text("Login")
